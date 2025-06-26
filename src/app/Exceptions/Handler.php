e8e327f1520af040dfcc5a07c95e9dff6661eded
k-saya <okk0543f787586n@ezweb.ne.jp>
2025-06-27 01:02:17 +0900

    L-7課題のPR

---
+++ src/app/Exceptions/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * Register exception handling callbacks.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+
+        // 不要なリダイレクトを避けるため下は使わない
+        // return redirect()->guest(route('login'));
+    }
+}
